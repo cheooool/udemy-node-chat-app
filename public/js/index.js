daf887d466231b285689d7ef74b5faf82bbcff9f
g.cheooool <g.cheooool@gmail.com>
2018-07-19 21:49:42 +0900

    heroku test

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,13 @@
-var socket = io();
+// var socket = io();
 
-socket.on('connect', function () {
-    console.log('Connected to server');
-});
+// socket.on('connect', function () {
+//     console.log('Connected to server');
+// });
 
-socket.on('disconnect', function () {
-    console.log('Disconnected from server');
-});
+// socket.on('disconnect', function () {
+//     console.log('Disconnected from server');
+// });
 
-socket.on('newMessage', function(message) {
-    console.log('newMesage', message);
-});
+// socket.on('newMessage', function(message) {
+//     console.log('newMesage', message);
+// });
--- server/server.js
+++ server/server.js
@@ -11,23 +11,23 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on('connection', (socket) => {
-    console.log('New user connected');
+// io.on('connection', (socket) => {
+//     console.log('New user connected');
 
-    socket.on('createMessage', (message) => {
-        console.log('createMessage', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createAt: new Date().getTime()
-        });
-    });
+//     socket.on('createMessage', (message) => {
+//         console.log('createMessage', message);
+//         io.emit('newMessage', {
+//             from: message.from,
+//             text: message.text,
+//             createAt: new Date().getTime()
+//         });
+//     });
 
-    socket.on('disconnect', () => {
-        console.log('User was disconnected');
-    });
-});
+//     socket.on('disconnect', () => {
+//         console.log('User was disconnected');
+//     });
+// });
 
-server.listen(port, () => {
+app.listen(port, () => {
     console.log('Server is up on port 3000');
 });
