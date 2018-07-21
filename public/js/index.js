2905297c5e809d5c124de25c2b54d2c4dfd6b4d3
g.cheooool <g.cheooool@gmail.com>
2018-07-21 13:41:57 +0900

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -28,13 +28,15 @@ socket.on('newLocationMessage', function(message) {
 });
 
 $('#message-form').on('submit', function(e) {
+    var $messageTextBox = $('[name=message]');
     e.preventDefault();
+
 
     socket.emit('createMessage', {
         from: 'User',
-        text: $('[name=message]').val()
+        text: $messageTextBox.val()
     }, function() {
-
+        $messageTextBox.val('');
     });
 });
 
@@ -44,13 +46,16 @@ $locationButton.on('click', function () {
         return alert('Geolocation not supported by your browser.');
     }
 
+    $locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
-        console.log(position.coords);
+        $locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        $locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.')
     });
 });
