b3cbeb1770733b0b4a982f38aa187ee6edf68525
g.cheooool <g.cheooool@gmail.com>
2018-07-22 22:24:25 +0900

    Add Users class and updateUserList event

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,6 @@
 var socket = io();
 
-function scrollToBottom() {
+function scrollToBottom () {
     // Selectors
     var $messages = $('#messages');
     var $newMessage = $messages.children('li:last-child');
@@ -18,14 +18,35 @@ function scrollToBottom() {
 }
 
 socket.on('connect', function () {
-    console.log('Connected to server');
+    var params = $.deparam(window.location.search);
+
+    console.log(params);
+
+    socket.emit('join', params, function (err) {
+        if (err) {
+            alert(err);
+            window.location.href = '/';
+        } else {
+            console.log('No error');
+        }
+    })
 });
 
 socket.on('disconnect', function () {
     console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function(message) {
+socket.on('updateUserList', function (users) {
+    var $ol = $('<ol></ol>');
+
+    users.forEach(function (user) {
+        $ol.append($('<li></li>').text(user));
+    });
+
+    $('#users').html($ol);
+});
+
+socket.on('newMessage', function (message) {
     var formattedTime = moment(message.createAt).format('h:mm a');
     var $template = $('#message-template').html();
     var html = Mustache.render($template, {
@@ -38,7 +59,7 @@ socket.on('newMessage', function(message) {
     scrollToBottom();
 });
 
-socket.on('newLocationMessage', function(message) {
+socket.on('newLocationMessage', function (message) {
     var formattedTime = moment(message.createAt).format('h:mm a');
     var $template = $('#location-message-template').html();
     var html = Mustache.render($template, {
@@ -51,7 +72,7 @@ socket.on('newLocationMessage', function(message) {
     scrollToBottom();
 });
 
-$('#message-form').on('submit', function(e) {
+$('#message-form').on('submit', function (e) {
     var $messageTextBox = $('[name=message]');
     e.preventDefault();
 
